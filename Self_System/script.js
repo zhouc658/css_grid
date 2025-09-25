c175909cf27cde77c12663ac15abea4703616681
zhouc658 <zhouc658@newschool.edu>
2025-09-25 11:48:11 -0400

    system js and asset

--- Self_System/script.js
+++ Self_System/script.js
@@ -1,19 +1,22 @@
 let inputText;      // input field where user types a number
-let category;       // dropdown to select "romantic" or "sad"
+let category;       // dropdown to select "romantic", "sad", "happy"
 
 let heart;          // heart image
 let droplet;        // droplet/square image
+let sun;            // sun image
 
 let redBkg, blueBkg;       // background images
 let redLine, redGlow;      // red line and glow
 let blueLine, blueGlow;    // blue line and glow
+let yellowLine, yellowGlow; // yellow line and glow
 
-let activeLine = "red";    // current page ("red" or "blue")
+let activeLine = "red";    // current page ("red", "blue", "yellow")
 
-// Stores data for red and blue pages
+// Stores data for red, blue, yellow pages
 let data = {
-  red: { heartTotal: 0, squareTotal: 0, bigHearts: [], smallHearts: [], bigDroplet: [], smallDroplet: [] },
-  blue: { heartTotal: 0, squareTotal: 0, bigHearts: [], smallHearts: [], bigDroplet: [], smallDroplet: [] }
+  red:    { heartTotal: 0, squareTotal: 0, bigHearts: [], smallHearts: [], bigDroplet: [], smallDroplet: [], bigSun: [], smallSun: [] },
+  blue:   { heartTotal: 0, squareTotal: 0, bigHearts: [], smallHearts: [], bigDroplet: [], smallDroplet: [], bigSun: [], smallSun: [] },
+  yellow: { heartTotal: 0, squareTotal: 0, bigHearts: [], smallHearts: [], bigDroplet: [], smallDroplet: [], bigSun: [], smallSun: [] }
 };
 
 function preload() {
@@ -25,8 +28,13 @@ function preload() {
   blueLine = loadImage("../asset/system/blue.PNG");
   blueGlow = loadImage("../asset/system/blueGlow.PNG");
 
+  yellowLine = loadImage("../asset/system/yellow.PNG");
+  yellowGlow = loadImage("../asset/system/yellowGlow.PNG");
+  yellowBkg= loadImage("../asset/system/yellowBkg.JPG")
+
   heart = loadImage("../asset/system/heart.webp");
   droplet = loadImage("../asset/system/droplet.webp");
+  sun = loadImage("../asset/system/sun.PNG"); // add your sun asset
 }
 
 function setup() {
@@ -36,16 +44,16 @@ function setup() {
   category = createSelect();
   category.option("romantic");
   category.option("sad");
-  category.position(950, height / 2.4);
+  category.option("happy"); // new category
+  category.position(980, height / 2.4);
   category.style('font-size', '18px');
   category.style('border-radius', '10px');
-  category.style('border', '2px solid #333');
   category.style('background-color', 'lightblue');
 
   // Input box
   inputText = createInput();
   inputText.size(110, 30);
-  inputText.position(950, height / 2.2);
+  inputText.position(970, height / 2.2);
   inputText.style('background-color', '#F7C9C0');
   inputText.style('border', '2px solid #A3BFD9');
   inputText.style('color', '#7A9BAE');
@@ -53,43 +61,50 @@ function setup() {
   inputText.style('border-radius', '10px');
 
   // Load totals from localStorage
-  data.red.heartTotal = Number(localStorage.getItem('red_heartTotal')) || 0;
-  data.red.squareTotal = Number(localStorage.getItem('red_squareTotal')) || 0;
-  data.blue.heartTotal = Number(localStorage.getItem('blue_heartTotal')) || 0;
-  data.blue.squareTotal = Number(localStorage.getItem('blue_squareTotal')) || 0;
+  for (let key of ["red", "blue", "yellow"]) {
+    data[key].heartTotal = Number(localStorage.getItem(`${key}_heartTotal`)) || 0;
+    data[key].squareTotal = Number(localStorage.getItem(`${key}_squareTotal`)) || 0;
+    data[key].sunTotal = Number(localStorage.getItem(`${key}_sunTotal`)) || 0;
 
-  // Initialize heart and droplet positions
-  updateHeartPositions(data.red);
-  updateDropletPositions(data.red);
-  updateHeartPositions(data.blue);
-  updateDropletPositions(data.blue);
+    updateHeartPositions(data[key]);
+    updateDropletPositions(data[key]);
+    updateSunPositions(data[key]);
+  }
 }
 
 function draw() {
   let page = data[activeLine];
 
-  // Background
-  background(activeLine === "red" ? redBkg : blueBkg);
+  // Backgrounds
+  if (activeLine === "red") background(redBkg);
+  if (activeLine === "blue") background(blueBkg);
+  if (activeLine === "yellow") background(yellowBkg); // placeholder if no image
 
   // Red line
-  if (page.heartTotal > page.squareTotal) {
+  if (page.heartTotal > page.squareTotal && page.heartTotal > (page.sunTotal || 0)) {
     image(redGlow, 0, 0, width, height / 1.9);
   } else {
     image(redLine, 0, 0, width, height / 1.9);
   }
 
   // Blue line
-  if (page.squareTotal > page.heartTotal) {
-    image(blueGlow, 0, 0, width, height / 1.8);
+  if (page.squareTotal > page.heartTotal && page.squareTotal > (page.sunTotal || 0)) {
+    image(blueGlow, 0, 0, width, height / 2);
   } else {
-    image(blueLine, 0, 0, width, height / 2.2);
+    image(blueLine, 0, 0, width, height / 2);
+  }
+
+  // Yellow line
+  if ((page.sunTotal || 0) > page.heartTotal && (page.sunTotal || 0) > page.squareTotal) {
+    image(yellowGlow, 0, 0, width, height / 2.5);
+  } else {
+    image(yellowLine, 0, 0, width, height / 2.5);
   }
 
   // Draw big hearts
   for (let pos of page.bigHearts) {
     image(heart, pos.x, pos.y, 100, 100);
   }
-
   // Draw small hearts
   for (let pos of page.smallHearts) {
     image(heart, pos.x, pos.y, 40, 40);
@@ -99,10 +114,18 @@ function draw() {
   for (let pos of page.bigDroplet) {
     image(droplet, pos.x, pos.y, 100, 100);
   }
-
   // Draw small droplets
   for (let pos of page.smallDroplet) {
     image(droplet, pos.x, pos.y, 40, 40);
+  }
+
+  // Draw big suns
+  for (let pos of page.bigSun) {
+    image(sun, pos.x, pos.y, 150, 150);
+  }
+  // Draw small suns
+  for (let pos of page.smallSun) {
+    image(sun, pos.x, pos.y, 60, 60);
   }
 }
 
@@ -110,33 +133,29 @@ function keyPressed() {
   if (keyCode === ENTER) {
     submit(); // add input
   } else if (key === " ") {
-    activeLine = activeLine === "red" ? "blue" : "red"; // switch pages
-  } else if (key === "C" || key === "c") {  // press C to clear all
-    // Reset data in memory
-    data.red.heartTotal = 0;
-    data.red.squareTotal = 0;
-    data.red.bigHearts = [];
-    data.red.smallHearts = [];
-    data.red.bigDroplet = [];
-    data.red.smallDroplet = [];
-
-    data.blue.heartTotal = 0;
-    data.blue.squareTotal = 0;
-    data.blue.bigHearts = [];
-    data.blue.smallHearts = [];
-    data.blue.bigDroplet = [];
-    data.blue.smallDroplet = [];
-
-    // Clear localStorage
-    localStorage.removeItem('red_heartTotal');
-    localStorage.removeItem('red_squareTotal');
-    localStorage.removeItem('blue_heartTotal');
-    localStorage.removeItem('blue_squareTotal');
-
+    // cycle pages red -> blue -> yellow
+    if (activeLine === "red") activeLine = "blue";
+    else if (activeLine === "blue") activeLine = "yellow";
+    else activeLine = "red";
+  } else if (key === "C" || key === "c") {
+    // Reset all
+    for (let key of ["red", "blue", "yellow"]) {
+      data[key].heartTotal = 0;
+      data[key].squareTotal = 0;
+      data[key].sunTotal = 0;
+      data[key].bigHearts = [];
+      data[key].smallHearts = [];
+      data[key].bigDroplet = [];
+      data[key].smallDroplet = [];
+      data[key].bigSun = [];
+      data[key].smallSun = [];
+      localStorage.removeItem(`${key}_heartTotal`);
+      localStorage.removeItem(`${key}_squareTotal`);
+      localStorage.removeItem(`${key}_sunTotal`);
+    }
     console.log("All data cleared!");
   }
 }
-
 
 function submit() {
   let text = inputText.value();
@@ -151,13 +170,17 @@ function submit() {
     } else if (choice === "sad") {
       page.squareTotal += number;
       updateDropletPositions(page);
+    } else if (choice === "happy") {
+      page.sunTotal = (page.sunTotal || 0) + number;
+      updateSunPositions(page);
     }
 
-    // Save totals to localStorage
-    localStorage.setItem('red_heartTotal', data.red.heartTotal);
-    localStorage.setItem('red_squareTotal', data.red.squareTotal);
-    localStorage.setItem('blue_heartTotal', data.blue.heartTotal);
-    localStorage.setItem('blue_squareTotal', data.blue.squareTotal);
+    // Save to localStorage
+    for (let key of ["red", "blue", "yellow"]) {
+      localStorage.setItem(`${key}_heartTotal`, data[key].heartTotal);
+      localStorage.setItem(`${key}_squareTotal`, data[key].squareTotal);
+      localStorage.setItem(`${key}_sunTotal`, data[key].sunTotal || 0);
+    }
   }
 
   inputText.value("");
@@ -166,29 +189,41 @@ function submit() {
 function updateHeartPositions(page) {
   let bigHeartsNeeded = Math.floor(page.heartTotal / 10);
   let smallHeartsNeeded = page.heartTotal % 10;
+  let margin = 40;
 
   while (page.bigHearts.length < bigHeartsNeeded) {
-    page.bigHearts.push({ x: random(100, width - 100), y: random(100, height - 100) });
+    page.bigHearts.push({ x: random(margin, width - 100 - margin), y: random(margin, height - 100 - margin) });
   }
   while (page.smallHearts.length < smallHeartsNeeded) {
-    page.smallHearts.push({ x: random(50, width - 50), y: random(50, height - 50) });
+    page.smallHearts.push({ x: random(margin, width - 40 - margin), y: random(margin, height - 40 - margin) });
   }
-  while (page.smallHearts.length > smallHeartsNeeded) {
-    page.smallHearts.pop();
-  }
+  while (page.smallHearts.length > smallHeartsNeeded) page.smallHearts.pop();
 }
 
 function updateDropletPositions(page) {
   let bigDropletNeeded = Math.floor(page.squareTotal / 10);
   let smallDropletNeeded = page.squareTotal % 10;
+  let margin = 40;
 
   while (page.bigDroplet.length < bigDropletNeeded) {
-    page.bigDroplet.push({ x: random(100, width - 100), y: random(100, height - 100) });
+    page.bigDroplet.push({ x: random(margin, width - 100 - margin), y: random(margin, height - 100 - margin) });
   }
   while (page.smallDroplet.length < smallDropletNeeded) {
-    page.smallDroplet.push({ x: random(50, width - 50), y: random(50, height - 50) });
+    page.smallDroplet.push({ x: random(margin, width - 40 - margin), y: random(margin, height - 40 - margin) });
   }
-  while (page.smallDroplet.length > smallDropletNeeded) {
-    page.smallDroplet.pop();
+  while (page.smallDroplet.length > smallDropletNeeded) page.smallDroplet.pop();
+}
+
+function updateSunPositions(page) {
+  let bigSunNeeded = Math.floor((page.sunTotal || 0) / 10);
+  let smallSunNeeded = (page.sunTotal || 0) % 10;
+  let margin = 40;
+
+  while (page.bigSun.length < bigSunNeeded) {
+    page.bigSun.push({ x: random(margin, width - 100 - margin), y: random(margin, height - 100 - margin) });
   }
+  while (page.smallSun.length < smallSunNeeded) {
+    page.smallSun.push({ x: random(margin, width - 40 - margin), y: random(margin, height - 40 - margin) });
+  }
+  while (page.smallSun.length > smallSunNeeded) page.smallSun.pop();
 }
